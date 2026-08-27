7b217d4c401bfe2af6061bda0eb9050e49904821
7odaAlaa <maahmoouudalaa2006@gmail.com>
2026-08-27 17:40:27 +0300

    Clone the Whole Project With Data Base Helper in Data Access Folder

---
+++ Resturaunt Manage Sysrem DataAccess Layer/DbHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturaunt_Manage_Sysrem_DataAccess_Layer
+{
+    public static class DbHelper
+    {
+        // Update the connection string to match your SQL Server instance
+        private static readonly string _connectionString =
+            "Server=localhost;Database=RestaurantDB;Integrated Security=True;";
+
+        public static SqlConnection CreateConnection()
+        {
+            return  new SqlConnection(_connectionString); 
+        }
+
+        public static SqlCommand CreateCommand(SqlConnection connection, string sql)
+        {
+            return new SqlCommand(sql, connection);
+        }
+
+        public static void AddParameter(SqlCommand command, string name, object value)
+        {
+            command.Parameters.AddWithValue(name, value ?? DBNull.Value);
+        }
+    }
+}
---
+++ Resturaunt Manage Sysrem DataAccess Layer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Resturaunt Manage Sysrem DataAccess Layer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Resturaunt Manage Sysrem DataAccess Layer")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8fac6027-4030-4c9f-9d81-fff7bf20225d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
